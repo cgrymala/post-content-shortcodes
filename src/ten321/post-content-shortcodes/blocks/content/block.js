519accf79a28b74a7edebe5315917fbc739a1a8f
Curtiss Grymala <cgrymala@umw.edu>
2020-08-12 11:42:21 -0400

    Attempt to get transforms working

--- src/ten321/post-content-shortcodes/blocks/content/block.js
+++ src/ten321/post-content-shortcodes/blocks/content/block.js
@@ -26,6 +26,46 @@ const {registerBlockType} = wp.blocks; // Import registerBlockType() from wp.blo
  *                             registered; otherwise `undefined`.
  */
 
+/**
+ * Get the value for a shortcode attribute, whether it's enclosed in double quotes, single
+ * quotes, or no quotes.
+ *
+ * @param  {string} tag     The shortcode name
+ * @param  {string} att     The attribute name
+ * @param  {string} content The text which includes the shortcode
+ *
+ * @return {string}         The attribute value or an empty string.
+ */
+export const getAttributeValue = function(tag, att, content){
+    // In string literals, slashes need to be double escaped
+    //
+    //    Match  attribute="value"
+    //    \[tag[^\]]*      matches opening of shortcode tag
+    //    att="([^"]*)"    captures value inside " and "
+    var re = new RegExp(`\\[${tag}[^\\]]* ${att}="([^"]*)"`, 'im');
+    var result = content.match(re);
+    if( result != null && result.length > 0 )
+        return result[1];
+
+    //    Match  attribute='value'
+    //    \[tag[^\]]*      matches opening of shortcode tag
+    //    att="([^"]*)"    captures value inside ' and ''
+    re = new RegExp(`\\[${tag}[^\\]]* ${att}='([^']*)'`, 'im');
+    result = content.match(re);
+    if( result != null && result.length > 0 )
+        return result[1];
+
+    //    Match  attribute=value
+    //    \[tag[^\]]*      matches opening of shortcode tag
+    //    att="([^"]*)"    captures a shortcode value provided without
+    //                     quotes, as in [me color=green]
+    re = new RegExp(`\\[${tag}[^\\]]* ${att}=([^\\s]*)\\s`, 'im');
+    result = content.match(re);
+    if( result != null && result.length > 0 )
+        return result[1];
+    return false;
+};
+
 let transformArgs = {};
 for (let i in ten321__post_content_shortcodes__blocks__content.reg_args.transforms.attributes) {
     if (!ten321__post_content_shortcodes__blocks__content.reg_args.transforms.attributes.hasOwnProperty(i)) {
@@ -58,13 +98,6 @@ registerBlockType('ten321--post-content-shortcodes--blocks/content', {
                     return /^\[post-content /.test(text);
                 },
                 transform: ({text}) => {
-
-                    const content = getInnerContent('post-content', text);
-
-                    const innerBlocks = wp.blocks.rawHandler({
-                        HTML: content,
-                    });
-
                     return wp.blocks.createBlock('ten321--post-content-shortcodes--blocks/content', {
                         id: getAttributeValue('post-content', 'id', text),
                         post_type: getAttributeValue('post-content', 'post-type', text),
